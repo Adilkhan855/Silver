e20664b2aa6ee38ad67b65ba75cfb6d2b55a9f1e
Adilkhan855 <adilhankadyrov712@gmail.com>
2025-04-10 15:30:39 +0600

    Update Convention II.cpp

--- Convention II.cpp
+++ Convention II.cpp
@@ -28,34 +28,6 @@ void setIO(string s) {
 	freopen((s + ".out").c_str(), "w", stdout);
 }
 
-const int inf = 1e15;
-
-struct SGT {
-	int n; vector<int> t;
-	
-	SGT(vector<int> a) : n(a.size()), t(n << 1) {
-		for(int i = 0; i < n; i++) t[i + n] = a[i];
-		for(int i = n - 1; i > 0; i--) {
-			t[i] = min(t[i << 1], t[i << 1 | 1]);
-		}
-	}
-	
-	void update(int i, int x) {
-		for(t[i += n] = x; i > 0; i >>= 1) {
-			t[i >> 1] = min(t[i], t[i ^ 1]);
-		}
-	}
-	
-	int get(int l, int r) {
-		int res = inf;
-		for(l += n, r += n + 1; l < r; l >>= 1, r >>= 1) {
-			if(l & 1) res = min(res, t[l++]);
-			if(r & 1) res = min(res, t[--r]);
-		}
-		return res;
-	}
-};
-
 void solve() {
 	setIO("convention2");
 	int n; cin >> n;
@@ -66,51 +38,32 @@ void solve() {
 		cin >> a[i] >> t[i];
 	}
 	
-	SGT s(a);
-	
-	queue<ar<int, 2>> q;
-	
-	int mn = *min_element(all(a));
+	vector<ar<int, 3>> cows;
 	
 	for(int i = 0; i < n; i++) {
-		if(a[i] == mn) {
-			q.push({i, a[i]});
-			break;
-		}
+		cows.pb({a[i], i, t[i]});
 	}
 	
-	set<pair<int, int>> st;
+	sort(all(cows));
 	
-	for(int i = 0; i < n; i++) st.emplace(a[i], i);
+	priority_queue<int, vector<int>, greater<int>> pq;
 	
-	int ans = 0;
+	int ans = 0, time = cows[0][0] + cows[0][2];
 	
-	while(!q.empty()) {
-		auto [i, T] = q.front();
-		q.pop();
-		
-		st.erase({a[i], i});
-		
-		T += t[i];
-		a[i] = inf;
-		s.update(i, inf);
-		
-		// cout << i + 1 << ' ' << T << nl;
-		
-		int l = 0, r = n - 1;
-		while(l < r) {
-			int mid = (l + r) >> 1;
-			if(s.get(0, mid) <= T) r = mid;
-			else l = mid + 1;
+	for(int i = 1; i < n || !pq.empty(); ) {
+		while(i < n && cows[i][0] <= time) {
+			pq.push(cows[i][1]);
+			i++;
 		}
-		
-		if(a[l] == inf) break;
-		
-		ans = max(ans, T - a[l]);
-		
-		if(a[l] > T) l = st.begin() -> second;
-		
-		q.push({l, max(T, a[l])});
+		if(!pq.empty()) {
+			int j = pq.top();
+			pq.pop();
+			ans = max(ans, time - a[j]);
+			time += t[j];
+		} else if(i < n) {
+			time = cows[i][0] + cows[i][2];
+			i++;
+		}
 	}
 	
 	cout << ans << nl;
