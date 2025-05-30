8a5baa33385571d29cfc8faf7334f57a1c339ee0
Adilkhan855 <adilhankadyrov712@gmail.com>
2025-05-30 19:17:18 +0600

    Add files via upload

--- Closing the Farm.cpp
+++ Closing the Farm.cpp
@@ -15,47 +15,67 @@ void setIO(string s) {
 	freopen((s + ".out").c_str(), "w", stdout);
 }
 
+struct dsu {
+	vector<int> p, sz;
+	
+	dsu(int n) : p(n), sz(n, 1) {
+		iota(all(p), 0);
+	}
+	
+	int find(int v) {
+		return v == p[v] ? v : p[v] = find(p[v]);
+	}
+	bool unite(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if(a == b) return false;
+		if(sz[a] < sz[b]) swap(a, b);
+		p[b] = a;
+		sz[a] += sz[b];
+		return true;
+	}
+};
+
 void solve() {
 	setIO("closing");
 	
 	int n, m; cin >> n >> m;
 	
-	vector<set<int>> G(n);
-	
-	for(int i = 0; i < m; i++) {
-		int u, v; cin >> u >> v;
+	vector<pair<int, int>> edges(m);
+	for(auto &[u, v] : edges) {
+		cin >> u >> v;
 		u--, v--;
-		G[u].emplace(v);
-		G[v].emplace(u);
 	}
 	
-	vector<int> used(n);
-	auto dfs = [&](auto &&dfs, int v) -> void {
-		used[v] = true;
-		for(auto to : G[v]) {
-			if(used[to]) continue;
-			dfs(dfs, to);
-		}
-	};
+	vector<int> p(n), pos(n);
 	
-	for(int it = 0; it < n; it++) {
-		used.assign(n, 0);
-		int comp = 0;
-		for(int i = 0; i < n; i++) {
-			if(!used[i]) {
-				dfs(dfs, i);
-				comp++;
-			}
-		}
-		cout << (comp - it == 1 ? "YES" : "NO") << nl;
-		int v; cin >> v;
-		v--;
-		for(auto to : G[v]) {
-			G[to].erase(v);
-		}
-		G[v].clear();
+	for(int i = 0; i < n; i++) {
+		cin >> p[i];
+		p[i]--;
+		pos[p[i]] = i;
 	}
 	
+	vector<vector<int>> G(n);
+	
+	for(auto [u, v] : edges) {
+		if(pos[u] > pos[v]) swap(u, v);
+		G[u].pb(v);
+	}
+	
+	dsu d(n);
+	
+	vector<int> ans(n);
+	int comps = 0;
+	for(int i = n - 1; i >= 0; i--) {
+		int u = p[i]; comps++;
+		for(auto v : G[u]) {
+			comps -= d.unite(u, v);
+		}
+		ans[i] = (comps <= 1);
+	}
+	for(int i = 0; i < n; i++) {
+		cout << (ans[i] ? "YES" : "NO") << nl;
+	}
 }
 
 signed main() {
